c5a5aad73f5c79d7e77076b60d2e9288232a9a66
Remus Lazar <remus@lazar.info>
2014-08-24 23:26:27 +0200

    initial commit

---
+++ init.el
@@ -0,0 +1,86 @@
+;; Remus Lazar's Emacs Setup
+;; =========================
+
+;; do package-install {jade,stylus,php}-mode, smart-tabs-mode
+;;
+
+(custom-set-variables
+
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(tool-bar-mode nil))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; key bindings
+(when (eq system-type 'darwin) ;; mac specific settings
+  (setq mac-option-key-is-meta nil)
+  (setq mac-option-modifier nil)
+  (setq mac-command-key-is-meta t)
+  (setq mac-command-modifier 'meta)
+  (global-set-key [kp-delete] 'delete-char) ;; sets fn-delete to be right-delete
+  )
+(iswitchb-mode 1)
+
+;; Some libs not available on MELPA (e.g. ts-mode)
+(add-to-list 'load-path "~/.emacs.d/vendor")
+
+;; Melpa Packages
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+(require 'stylus-mode)
+(require 'jade-mode)
+(add-to-list 'auto-mode-alist '("\\.styl$" . stylus-mode))
+(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+;; Don't use TABs by detault
+;; Do use TABs only for some specific modes
+(setq-default indent-tabs-mode nil)
+
+;; JavaScript: indend-level is 2, use spaces, no TABs
+(setq js-indent-level 2)
+
+;; TYPO3 TypoScript, do use TABs, TAB-width is 4
+(require 'ts-mode)
+(add-to-list 'auto-mode-alist '("\\.ts$" . ts-mode))
+(add-hook 'ts-mode-common-hook
+          (lambda () (setq indent-tabs-mode t)))
+(add-hook 'ts-mode-common-hook
+          (lambda () (setq tab-width 4)))
+
+;; C, PHP, indent with tabs, TAB width is 4
+(add-hook 'c-mode-common-hook
+          (lambda () (setq indent-tabs-mode t)))
+(add-hook 'c-mode-common-hook
+          (lambda () (setq tab-width 4)))
+
+;; Use smart-tabs for this major modes (nxml for PHP-Mode)
+(smart-tabs-insinuate 'c 'c++ 'java 'javascript 'cperl 'python
+                      'ruby 'nxml)
+
+;; HTML tab-width 2, indend with TABs using Smart Tabs
+(add-hook 'html-mode-common-hook
+		  (lambda () (setq indent-tabs-mode t)))
+(add-hook 'html-mode-common-hook
+		  (lambda () (setq tab-width 2)))
+(add-hook 'nxml-mode-common-hook
+		  (lambda () (setq tab-width 2)))
+
+;; Auto cleanup all the whitespace before save.
+;; Cleanup only the trailing spaces, dont convert
+;; spaces to tabs and vice-versa
+(setq whitespace-style (quote
+                        ( empty trailing )))
+
+(add-hook 'before-save-hook 'whitespace-cleanup)
