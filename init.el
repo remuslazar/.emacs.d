2a6d591768de9d6678289dd460ab96ff7c9838e3
Remus Lazar <remus@lazar.info>
2014-08-24 23:53:05 +0200

    nxml-slash-autocomplete-flag on

--- init.el
+++ init.el
@@ -5,12 +5,12 @@
 ;;
 
 (custom-set-variables
-
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
+ '(nxml-slash-auto-complete-flag t)
  '(tool-bar-mode nil))
 
 (custom-set-faces
